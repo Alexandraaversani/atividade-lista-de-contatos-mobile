203b013b4d408589788cbc27074f12c887e5bce2
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2025-05-15 13:56:29 -0300

    feat: criação settings

---
+++ MyContacts/app/settings.js
@@ -0,0 +1,26 @@
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-paper';
+
+export default function Settings() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Configurações</Text>
+            <Text style={styles.text}>Versão 1.0.0</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+    text: {
+        fontSize: 16, 
+    }
+});
